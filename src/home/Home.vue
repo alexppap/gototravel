cf1b4971fa9b8ab61eb0d564a567d448ca244c95
alexppap <1060788599@qq.com>
2020-02-02 17:42:17 +0800

    ready

---
+++ src/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="stylus" scoped>
+.header
+  font-size:30px
+</style>
