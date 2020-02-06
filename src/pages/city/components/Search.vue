b6fb797f3a6a7bdbbe0012855f3bc42b7c713652
alexppap <1060788599@qq.com>
2020-02-06 17:29:57 +0800

    flexcity

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="search">
+      <input class="search-input" type="text" placeholder="请输入你想搜索的城市">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variable.styl'
+.search
+  height: .72rem
+  padding: 5px
+  background: $bgColor
+  .search-input
+    box-sizing: border-box
+    text-align: center
+    padding: 0 .2rem
+    width: 100%
+    height: .62rem
+    line-height: .62rem
+    border-radius: .06rem
+    background: #fff
+    color: #666
+</style>
