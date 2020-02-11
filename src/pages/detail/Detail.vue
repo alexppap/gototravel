d0f4969a4db20c68f38b68e2bb4a038736dd4379
alexppap <1060788599@qq.com>
2020-02-11 20:55:17 +0800

    baseinfo

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,17 +2,20 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-baseinfo></detail-baseinfo>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailBaseinfo from './components/Baseinfo'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailBaseinfo
   }
 }
 </script>
---
+++ src/pages/detail/components/Baseinfo.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="content">
+    <div class="baseinfo">
+      <div class="img-wrapper">
+        <div>
+          <span class="mp-score">4.9</span>
+          <span class="mp-text">分</span>
+          <span class="mp-desc">很棒</span>
+        </div>
+        <div>
+          <span class="mp-totalcommentnum">5100条评论</span>
+          <span class="mp-totalcommentnum">22条攻略</span>
+        </div>
+        <div class="iconfont right-arrow">&#xe62d;</div>
+      </div>
+      <div class="img-wrapper">
+        <div><span class="mp-title">景点简介</span></div>
+        <div><span class="mp-text2">开放时间、贴士</span></div>
+        <div class="iconfont right-arrow">&#xe62d;</div>
+      </div>
+    </div>
+    <div class="title-wrapper">
+      <div class="text-title">北京市西城区景山西街44号</div>
+      <div class="iconfont right-arrow">&#xe62d;</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBaseinfo'
+}
+</script>
+
+<style lang="stylus" scoped>
+.content
+  position: relative
+  top: -0.1rem
+  height: 1.88rem
+  border-radius: .1rem .1rem 0 0
+  padding: .1rem .2rem 0 .2rem
+  .baseinfo
+    height: 50%
+    .img-wrapper
+      float: left
+      position: relative
+      width: 50%
+      height: 100%
+      padding-top: .1rem
+.right-arrow
+  position: absolute
+  top: .2rem
+  right: .2rem
+  color: #9e9e9e
+  font-size: .36rem
+.mp-score
+  font-size: .44rem
+  color: #ff8300
+  line-height: .44rem
+.mp-text
+  font-size: .28rem
+  color: #ff8300
+  line-height: .48rem
+.mp-desc
+  font-size: .28rem
+  color: #ff8300;
+  line-height: .48rem
+  margin-left: .2rem
+.mp-totalcommentnum
+  font-size: .24rem
+  color: #9e9e9e;
+  line-height: .32rem
+  margin-right: .2rem
+.mp-title
+  font-size: .28rem
+  color: #212121
+  line-height: .48rem
+  padding-left: .3rem
+.mp-text2
+  color: #9e9e9e
+  font-size: .24rem
+  padding-left: .3rem
+.title-wrapper
+  padding: .2rem 0
+  position: relative
+  .text-title
+    text-indent: .2rem
+</style>
